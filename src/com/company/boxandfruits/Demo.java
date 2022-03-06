75124b9667e56a33ea812f34b72daffc918bd3b2
anasonov <agnason@gmail.com>
2022-03-06 18:09:26 +0300

    Lesson01_is ready

---
+++ src/com/company/boxandfruits/Demo.java
@@ -0,0 +1,24 @@
+package com.company.boxandfruits;
+
+import java.util.ArrayList;
+
+public class Demo {
+    public static void main(String[] args) {
+        Orange orange = new Orange(20);
+        Orange orange2 = new Orange(10);
+        Apple apple = new Apple(45);
+
+        Box<Orange> boxOrange = new Box(new ArrayList<>(), "апельсин");
+        Box<Orange> boxOrange1 = new Box(new ArrayList<>(), "апельсин");
+        Box<Apple> boxApple = new Box(new ArrayList<>(), "яблоко");
+        Box<Apple> boxApple1 = new Box(new ArrayList<>(), "яблоко");
+
+        boxOrange.add(orange);
+        boxOrange.add(orange2);
+
+        boxApple1.add(apple);
+        boxApple.pass(boxApple1);
+
+        System.out.println(boxApple.sameAvg(boxOrange));
+    }
+}
