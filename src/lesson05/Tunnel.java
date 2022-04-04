2e3e167d513c28e21d920e729f444593217f3d21
anasonov <agnason@gmail.com>
2022-04-04 20:13:24 +0300

    lesson05_final

--- src/lesson05/Tunnel.java
+++ src/lesson05/Tunnel.java
@@ -1,7 +1,12 @@
 package lesson05;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+    private Semaphore smp;
+
     public Tunnel() {
+        smp = new Semaphore(MainClass.CARS_COUNT / 2);
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
@@ -9,17 +14,19 @@ public class Tunnel extends Stage {
     @Override
     public void go(Car c) {
         try {
-            try {
+            if (!smp.tryAcquire()) {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-                System.out.println(c.getName() + " начал этап: " + description);
-                Thread.sleep(length / c.getSpeed() * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(c.getName() + " закончил этап: " + description);
+                smp.acquire();
             }
-        } catch (Exception e) {
+
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println(c.getName() + " закончил этап: " + description);
+            smp.release();
         }
     }
 }
+
